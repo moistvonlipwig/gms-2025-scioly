ffd75b24d12dc5fee5965a5bfe0c53e11083fabb
Chris Teli <christopher.r.teli.civ@us.navy.mil>
2025-01-27 05:03:21 +0000

    Update data for beta-1

--- src/quizzes/MicrobeMission.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const MicrobeMission = () => {
-  return <h2>Microbe Mission Quiz Coming Soon!</h2>;
-};
-
-export default MicrobeMission;
-
--- src/quizzes/Optics.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Optics = () => {
-  return <h2>Optics Quiz Coming Soon!</h2>;
-};
-
-export default Optics;
